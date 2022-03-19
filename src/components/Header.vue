dd81133ccdfc5657b36608a257f1fe753aa2fa11
Muritala David <54484130+Fn-studyo@users.noreply.github.com>
2022-03-19 14:24:32 +0100

    added new company

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,14 +15,8 @@
             <button id="dropdownButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Companies<svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
             <div id="dropdown" class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
             <ul class="py-1" aria-labelledby="Companies">
-            <li>
-            <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">BrimbleHQ</a>
-            </li>
-            <li>
-            <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">LoftNG</a>
-            </li>
-            <li>
-            <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Ebhidda Services</a>
+            <li v-for="c in companies" :key="c.url">
+            <a :href="c.url" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{{ c.name }}</a>
             </li>
             </ul>
             </div>
@@ -61,6 +55,9 @@ export default {
             },{
                 name: 'LoftNG',
                 url: 'http://myloftapp.co/'
+            },{
+              name: 'Africa Fintech LTD',
+              url: '#'
             }]
         }
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
           <h1 class="mb-6 text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight">Meet Ileri ⚡️.</h1>
           <p class="mb-8 text-lg md:text-xl text-coolGray-500 font-medium">Hello, I'm Muritala David Ilerioluwa, a Backend Developer based in Lagos.</p>
           <div class="flex flex-wrap">
-            <div class="w-full md:w-auto py-1 md:py-0 md:mr-4"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm" href="https://drive.google.com/file/d/1czVHvgGA4vZKwngmXETIwUfD4F-ag2Xk/view?usp=sharing" target="_blank" download>Resume</a></div>
+            <div class="w-full md:w-auto py-1 md:py-0 md:mr-4"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm" href="https://drive.google.com/file/d/1czVHvgGA4vZKwngmXETIwUfD4F-ag2Xk/view?usp=sharing" target="_blank">Resume</a></div>
             <div class="w-full md:w-auto py-1 md:py-0"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-coolGray-800 font-medium text-center bg-white hover:bg-coolGray-100 focus:ring-2 focus:ring-coolGray-200 focus:ring-opacity-50 border border-coolGray-200 rounded-md shadow-sm" href="mailto:davmuri1414@gmail.com">Contact</a></div>
           </div>
         </div>
